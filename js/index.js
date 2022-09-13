d109ba0d1b33cb75b46a7625fa230b050a20bfc6
LilianaFranco <franco0123@gmail.com>
2022-09-13 17:50:39 -0500

    Cycle a Json file after parsing

---
+++ js/index.js
@@ -0,0 +1,53 @@
+const jsonData = `
+{
+  "squadName": "Super hero squad",
+  "homeTown": "Metro City",
+  "formed": 2016,
+  "secretBase": "Super tower",
+  "active": true,
+  "members": [
+    {
+      "name": "Molecule Man",
+      "age": 29,
+      "secretIdentity": "Dan Jukes",
+      "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
+`;
+
+//Recorrer el JSON e insertar en el listado del html los nombres de los miembros
+function recorrerMiembros() {
+    const heroesSquad = JSON.parse(jsonData);
+    let heroes = heroesSquad.members;
+    console.log(heroes);
+    for (const iterator in heroes) {
+      const li = document.createElement("li");
+      const parentList = document.getElementById("miembros")
+      parentList.appendChild(li)
+      li.innerText = heroes[iterator].name
+    }
+};
+recorrerMiembros(); 
